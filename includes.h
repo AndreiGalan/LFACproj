20e4d1bd374498eb65927db6af2f88efc5546a34
fiodorov <cristifv59@gmail.com>
2022-12-28 12:34:43 +0200

    added includes.h

---
+++ includes.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H 
+
+typedef union Value{
+	int valINT;
+	float valFLOAT;
+	char valCHAR;
+	char* valSTRING;
+	int valBOOL;
+} Value;
+
+
+typedef struct Variable{
+	char name[50];
+	int type;
+	int is_const;
+	union Value value;
+} Variable;
+
+#endif
